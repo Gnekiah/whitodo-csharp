50e1d1e89bb95e4acf0a68ecc50708fc8a37079a
Xxiong <xxiong@cqu.edu.cn>
2017-10-22 16:41:16 +0800

    runned

--- whitodo-csharp/Properties/Resources.Designer.cs
+++ whitodo-csharp/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace whitodo_csharp.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
-        /// </summary>
-        internal static System.Drawing.Bitmap whitodo {
-            get {
-                object obj = ResourceManager.GetObject("whitodo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
